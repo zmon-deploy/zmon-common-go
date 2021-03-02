fcad347514c94ae5be379f81c2a1a406f80c4291
이재용 <jaeyo.developer@gmail.com>
2021-03-02 10:03:54 +0900

    retry.DoWithDelay 추가

--- misc/retry.go
+++ misc/retry.go
@@ -1,5 +1,7 @@
 package misc
 
+import "time"
+
 type retryer struct {
 	retryCount int
 }
@@ -7,10 +9,16 @@ type retryer struct {
 type retryFn func() bool
 
 func (r *retryer) Do(callback retryFn) {
+	r.DoWithDelay(callback, 0)
+}
+
+func (r *retryer) DoWithDelay(callback retryFn, delay time.Duration) {
 	for i := 0; i < r.retryCount; i++ {
 		success := callback()
 		if success || i >= r.retryCount-1 {
 			return
+		} else {
+			time.Sleep(delay)
 		}
 	}
 }
