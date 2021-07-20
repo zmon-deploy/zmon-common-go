5579523cc4682240e2c3224133c95e6a33f86dca
Jaeyo <jaeyo.developer@gmail.com>
2021-07-20 11:24:49 +0900

    PostgreSQL 연동

--- database/database_adapter.go
+++ database/database_adapter.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/pkg/errors"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
 )
@@ -12,7 +13,7 @@ type DatabaseAdapter struct {
 	*gorm.DB
 }
 
-func NewDatabaseAdapter(server, user, password string, port int, database string, minConn, maxConn int, useLocalTime bool) (*DatabaseAdapter, error) {
+func NewMySQLDatabaseAdapter(server, user, password string, port int, database string, minConn, maxConn int, useLocalTime bool) (*DatabaseAdapter, error) {
 	dataSource := fmt.Sprintf(
 		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True",
 		user,
@@ -25,7 +26,30 @@ func NewDatabaseAdapter(server, user, password string, port int, database string
 		dataSource = dataSource + "&loc=Local"
 	}
 
-	conn, err := gorm.Open(mysql.Open(dataSource), &gorm.Config{
+	return newDatabaseAdapter(mysql.Open(dataSource), minConn, maxConn)
+}
+
+func NewPostgresDatabaseAdapter(server, user, password string, port int, database string, minConn, maxConn int, timeZonePtr *string) (*DatabaseAdapter, error) {
+	timeZone := "Asia/Seoul"
+	if timeZonePtr != nil {
+		timeZone = *timeZonePtr
+	}
+
+	dataSource := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
+		server,
+		user,
+		password,
+		database,
+		port,
+		timeZone,
+	)
+
+	return newDatabaseAdapter(postgres.Open(dataSource), minConn, maxConn)
+}
+
+func newDatabaseAdapter(dialector gorm.Dialector, minConn, maxConn int) (*DatabaseAdapter, error) {
+	conn, err := gorm.Open(dialector, &gorm.Config{
 		Logger: gormlogger.Discard,
 	})
 	if err != nil {
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/etcd-io/bbolt v1.3.3
 	github.com/influxdata/influxdb1-client v0.0.0-20200827194710-b269163b24ab
 	github.com/influxdata/line-protocol v0.0.0-20201012155213-5f565037cbc9
+	github.com/jackc/pgx/v4 v4.12.0 // indirect
 	github.com/klauspost/compress v1.11.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/echo/v4 v4.1.17
@@ -15,13 +16,13 @@ require (
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
 	github.com/segmentio/kafka-go v0.4.16
 	github.com/sirupsen/logrus v1.7.0
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
 	go.etcd.io/bbolt v1.3.5 // indirect
 	go.uber.org/multierr v1.6.0
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 	gorm.io/driver/mysql v1.0.4
-	gorm.io/gorm v1.21.3
+	gorm.io/driver/postgres v1.1.0
+	gorm.io/gorm v1.21.12
 )
