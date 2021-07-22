3436486378d69cef21f670fef072c70b7a13486d
Jaeyo <jaeyo.developer@gmail.com>
2021-07-22 09:22:36 +0900

    aes 암복호화 함수에 panic 에 대한 예외처리 추가

--- crypto/aes.go
+++ crypto/aes.go
@@ -4,29 +4,33 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
+	"fmt"
+	"github.com/pkg/errors"
 	"strings"
 )
 
 type AESCipher struct {
-	cipher cipher.Block
+	secretKey string
 }
 
-func NewAESCipher(password string) *AESCipher {
-	pass := []byte(password)
-	secret := make([]byte, 16)
-	copy(secret, pass)
-	for i := 16; i < len(pass); {
-		for j := 0; j < 16 && i < len(pass); j, i = j+1, i+1 {
-			secret[j] ^= pass[i]
-		}
-	}
-	cipher, _ := aes.NewCipher(secret)
+func NewAESCipher(secretKey string) *AESCipher {
 	return &AESCipher{
-		cipher: cipher,
+		secretKey: secretKey,
 	}
 }
 
-func (t *AESCipher) AESEncrypt(srcstr string) string {
+func (t *AESCipher) AESEncrypt(srcstr string) (out string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic: r")
+		}
+	}()
+
+	cipherBlock, err := getCipher(t.secretKey)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get cipher")
+	}
+
 	src := []byte(srcstr)
 	length := (len(src) + aes.BlockSize) / aes.BlockSize
 	plain := make([]byte, length*aes.BlockSize)
@@ -37,18 +41,33 @@ func (t *AESCipher) AESEncrypt(srcstr string) string {
 	}
 	encrypted := make([]byte, len(plain))
 
-	for bs, be := 0, t.cipher.BlockSize(); bs <= len(src); bs, be = bs+t.cipher.BlockSize(), be+t.cipher.BlockSize() {
-		t.cipher.Encrypt(encrypted[bs:be], plain[bs:be])
+	for bs, be := 0, cipherBlock.BlockSize(); bs <= len(src); bs, be = bs+cipherBlock.BlockSize(), be+cipherBlock.BlockSize() {
+		cipherBlock.Encrypt(encrypted[bs:be], plain[bs:be])
 	}
 
-	return strings.ToUpper(hex.EncodeToString(encrypted))
+	return strings.ToUpper(hex.EncodeToString(encrypted)), nil
 }
 
-func (t *AESCipher) AESDecrypt(hexstr string) string {
-	encrypted, _ := hex.DecodeString(hexstr)
+func (t *AESCipher) AESDecrypt(hexstr string) (out string, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic: r")
+		}
+	}()
+
+	cipherBlock, err := getCipher(t.secretKey)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get cipher")
+	}
+
+	encrypted, err := hex.DecodeString(hexstr)
+	if err != nil {
+		return "", err
+	}
+
 	decrypted := make([]byte, len(encrypted))
-	for bs, be := 0, t.cipher.BlockSize(); bs < len(encrypted); bs, be = bs+t.cipher.BlockSize(), be+t.cipher.BlockSize() {
-		t.cipher.Decrypt(decrypted[bs:be], encrypted[bs:be])
+	for bs, be := 0, cipherBlock.BlockSize(); bs < len(encrypted); bs, be = bs+cipherBlock.BlockSize(), be+cipherBlock.BlockSize() {
+		cipherBlock.Decrypt(decrypted[bs:be], encrypted[bs:be])
 	}
 
 	trim := 0
@@ -56,5 +75,17 @@ func (t *AESCipher) AESDecrypt(hexstr string) string {
 		trim = len(decrypted) - int(decrypted[len(decrypted)-1])
 	}
 
-	return string(decrypted[:trim])
+	return string(decrypted[:trim]), nil
+}
+
+func getCipher(secretKey string) (cipher.Block, error) {
+	pass := []byte(secretKey)
+	secret := make([]byte, 16)
+	copy(secret, pass)
+	for i := 16; i < len(pass); {
+		for j := 0; j < 16 && i < len(pass); j, i = j+1, i+1 {
+			secret[j] ^= pass[i]
+		}
+	}
+	return aes.NewCipher(secret)
 }
--- crypto/aes_test.go
+++ crypto/aes_test.go
@@ -7,7 +7,18 @@ import (
 
 func TestCrypto(t *testing.T) {
 	crypto := NewAESCipher("abcdef123456")
-	encrypted := crypto.AESEncrypt("helloWorld")
-	decrypted := crypto.AESDecrypt(encrypted)
+
+	encrypted, err := crypto.AESEncrypt("helloWorld")
+	require.NoError(t, err)
+
+	decrypted, err := crypto.AESDecrypt(encrypted)
+	require.NoError(t, err)
+
 	require.Equal(t, "helloWorld", decrypted)
+}
+
+func TestWrongEncrypted(t *testing.T) {
+	wrongEncrypted := "ca8e81c53958038846775d6f00c46df086e601d33bd0af78e1b4d396e90e42afe330e53787ea0832e3562e7e718feb71"
+	_, err := NewAESCipher("hseRTo5bUFhdeI9W").AESDecrypt(wrongEncrypted)
+	require.Error(t, err)
 }
